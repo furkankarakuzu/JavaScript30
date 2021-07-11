3cfad9120f081b4959c144b390527acb5ee92e53
Furkan Karakuzu <furkannkarakuzu@hotmail.com>
2021-07-12 00:16:00 +0300

    Day-3 added

---
+++ Day3-PlayingWithCssVariablesAndJs/main.js
@@ -0,0 +1,17 @@
+function update(e){
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + (this.dataset.sizing || ""));
+}
+
+const values = document.querySelectorAll('.controls input')
+
+values.forEach(element => {
+    element.addEventListener('change',update);
+});
+
+values.forEach(element=>{
+    element.addEventListener('mousemove',update);
+})
+
+values.forEach(element=>{
+    element.addEventListener('onchange',update);
+})
